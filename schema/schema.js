79395cf9ed1d78301dcaad42c28c3807a5097043
rafaelbchs <rafaelbchacins@gmail.com>
2022-09-29 10:27:38 -0500

    added schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,13 +1,43 @@
-const graphql = require('graphql');
-const {
-    GraphQLObjectType, GraphQLString, GraphQLInt
-} = graphql;
+const graphql = require("graphql");
+const _ = require("lodash");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+  {
+    id: "23",
+    firstName: "Bill",
+    age: 20,
+  },
+  {
+    id: "47",
+    firstName: "Samantha",
+    age: 21,
+  },
+];
 
 const UserType = new GraphQLObjectType({
-    name: 'User', 
-    fields: {
-        id: {type: GraphQLString},
-        firstName: {type: GraphQLString},
-        age: {type: GraphQLInt} 
-    }
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, {id: args.id})
+      },
+    },
+  },
+});
+
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 });
